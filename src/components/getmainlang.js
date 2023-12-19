9cde49e699abe9db4fb21919c305201dab4f03d1
oswiil <jfosanchez@gmail.com>
2023-12-19 21:15:02 +0100

    refreshtoken

---
+++ src/components/getmainlang.js
@@ -0,0 +1,3 @@
+import github from '../utils/axios';
+
+github.get(`https://api.github.com/repos/~{}dotnet/corefx/languages`);
--- src/utils/axios.js
+++ src/utils/axios.js
@@ -2,10 +2,11 @@ import axios from 'axios';
 
 const github = axios.create({ baseURL: 'https://api.github.com' });
 
-// let accessToken = null;
+let accessToken = null;
 
 github.interceptors.request.use(async (config) => {
-  config.headers.Authorization = `Bearer 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`;
+  config.headers.Authorization =
+    `Bearer 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` || accessToken;
   config.headers['Content-Type'] = 'application/json';
   config.headers['X-GitHub-Api-Version'] = '2022-11-28';
   config.headers['Accept'] = 'application/vnd.github+json';
@@ -17,26 +18,29 @@ github.interceptors.response.use(async (config) => {
     '🚀 ~ file: axios.js:18 ~ github.interceptors.response.use ~ config:',
     config
   );
+  if (config.status !== 200) {
+    renewtoken();
+  }
 
   return config;
 });
 
-// let renewtoken = async () => {
-//   if (!accessToken) {
-//     try {
-//       const response = await github.get('https://api.github.com/octocat', {
-//         headers: {
-//           Authorization: `Bearer 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`,
-//         },
-//       });
-//       accessToken = response.data.access_token;
-//     } catch (error) {
-//       console.error(
-//         'Error obtaining access token:',
-//         error.response ? error.response.data : error.message
-//       );
-//       throw error;
-//     }
-//   }
-// };
+let renewtoken = async () => {
+  if (!accessToken) {
+    try {
+      const response = await github.get('https://api.github.com/octocat', {
+        headers: {
+          Authorization: `Bearer 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`,
+        },
+      });
+      accessToken = response.data.access_token;
+    } catch (error) {
+      console.error(
+        'Error obtaining access token:',
+        error.response ? error.response.data : error.message
+      );
+      throw error;
+    }
+  }
+};
 export default github;
